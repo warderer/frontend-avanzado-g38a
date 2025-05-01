80f43f4953248619a2becd4d2502b2ab86b4ba3d
César Guerra <warderer@gmail.com>
2025-04-30 21:09:30 -0600

    ✨ Add Provider to Home component

--- 02-react-context/src/pages/Home/Home.jsx
+++ 02-react-context/src/pages/Home/Home.jsx
@@ -1,19 +1,22 @@
 import SongList from '@/components/SongList'
 import SongDetails from '@/components/SongDetails'
+import { SongProvider } from '@/context/SongContext'
 import './home.css'
 
 const Home = () => {
   return (
-    <div className='home-container'>
-      <div className='izquierdo'>
-        <h2>Lado Izquierdo</h2>
-        <SongList />
-      </div>
+    <SongProvider>
+      <div className='home-container'>
+        <div className='izquierdo'>
+          <h2>Lado Izquierdo</h2>
+          <SongList />
+        </div>
 
-      <div className='derecho'>
-        <SongDetails />
+        <div className='derecho'>
+          <SongDetails />
+        </div>
       </div>
-    </div>
+    </SongProvider>
   )
 }
 export default Home
