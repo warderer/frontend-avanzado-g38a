4dcdf8791df5aceaf5e61a5e84b8c7997df5c465
César Guerra <warderer@gmail.com>
2025-04-30 20:34:06 -0600

    ✨ Add Songs to SongList component

--- 02-react-context/src/components/SongList/SongList.jsx
+++ 02-react-context/src/components/SongList/SongList.jsx
@@ -1,8 +1,31 @@
+import { useState, useEffect } from 'react'
+import canciones from '@/assets/listaCanciones.json'
 import './songlist.css'
 
 const SongList = () => {
+  const [songs, setSongs] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  // Simulo una llamada a una API
+  useEffect(() => {
+    setTimeout(() => {
+      setSongs(canciones)
+      setLoading(false)
+    }, 2000)
+  }, [])
+
   return (
-    <div>SongList</div>
+    <section className='row-cointainer'>
+      {
+            loading
+              ? <h2> Cargando...</h2>
+              : songs.map((song) => (
+                <div className='row-song' key={song.id}>
+                  <h3>{song.title}</h3>
+                  <p>{song.artist}</p>
+                </div>))
+        }
+    </section>
   )
 }
 export default SongList
