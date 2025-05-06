0b3fe351abc526b16d2af8caf2a81645c776278d
César Guerra <warderer@gmail.com>
2025-05-05 20:32:45 -0600

    ✨ Add Header Basic Menu Structure

--- 03-react-auth/src/components/Header/Header.jsx
+++ 03-react-auth/src/components/Header/Header.jsx
@@ -1,6 +1,26 @@
 const Header = () => {
   return (
-    <div>Header</div>
+    <nav className=''>
+      <a href='/' className=''>LOGO</a>
+
+      <ul className=''>
+        <li className=''>
+          <a href='/' className=''>Home</a>
+        </li>
+        <li className=''>
+          <a href='/dashboard' className=''>Dashboard</a>
+        </li>
+        <li className=''>
+          <a href='/secret' className=''>Secret</a>
+        </li>
+        <li className=''>
+          <a href='/login' className=''>Login</a>
+        </li>
+        <li className=''>
+          <a href='/signup' className=''>Signup</a>
+        </li>
+      </ul>
+    </nav>
   )
 }
 export default Header
