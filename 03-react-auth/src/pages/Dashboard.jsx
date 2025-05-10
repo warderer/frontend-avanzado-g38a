b30be1f7a0c80f86782259e70218dbb9090e1856
César Guerra <warderer@gmail.com>
2025-05-09 21:17:33 -0600

    ✨ Add userProfile data in Dashboard

--- 03-react-auth/src/pages/Dashboard.jsx
+++ 03-react-auth/src/pages/Dashboard.jsx
@@ -1,6 +1,36 @@
+import { useState, useEffect } from 'react'
+import { getMeUserService } from '@/services/userServices'
+
 const Dashboard = () => {
+  const [user, setUser] = useState(null)
+  const token = localStorage.getItem('token')
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const { status, data } = await getMeUserService(token)
+        if (status === 200) {
+          setUser(data)
+        }
+      } catch (error) {
+        console.error('Error fetching user data:', error)
+      }
+    }
+
+    if (token) {
+      fetchUser()
+    }
+  }, [token])
+
   return (
-    <h1>Dashboard</h1>
+    <>
+      <h1>Dashboard</h1>
+      {user?.first_name && <h4>Nombre: {user.first_name}</h4>}
+      {user?.last_name && <h4>Apellido: {user.last_name}</h4>}
+      {user?.gender && <h4>Genero: {user.gender}</h4>}
+      {user?.email && <h4>Email: {user.email}</h4>}
+      {user?.role && <h4>Role: {user.role}</h4>}
+    </>
   )
 }
 export default Dashboard
--- 03-react-auth/src/services/userServices.js
+++ 03-react-auth/src/services/userServices.js
@@ -4,8 +4,14 @@ const BASE_URL = 'https://ecommerce-json-jwt.onrender.com'
 
 const registerUserService = (data) => axios.post(`${BASE_URL}/register`, data)
 const loginUserService = (data) => axios.post(`${BASE_URL}/login`, data)
+const getMeUserService = (token) => axios.get(`${BASE_URL}/users/me`, {
+  headers: {
+    Authorization: `Bearer ${token}`
+  }
+})
 
 export {
   registerUserService,
-  loginUserService
+  loginUserService,
+  getMeUserService
 }
